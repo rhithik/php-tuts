c23bfa1c1ca9d682e58c4993abfb2931ca95392d
rhithik <rhithiktheawesome@gmail.com>
2020-11-02 08:50:27 -0500

    added some basic validation

--- add.php
+++ add.php
@@ -1,9 +1,29 @@
 <?php
 
 if (isset($_POST['submit'])){
-  echo htmlspecialchars($_POST['email']);
-  echo htmlspecialchars($_POST['title']);
-  echo htmlspecialchars($_POST['ingredients']);
+	//check email
+	if(empty($_POST['email'])){
+		echo 'An email is required <br />';
+	} else {
+    echo htmlspecialchars($_POST['email']);
+	}
+
+	//check title
+  if(empty($_POST['title'])){
+    echo 'A title is required <br />';
+  } else {
+    echo htmlspecialchars($_POST['title']);
+  }
+
+  //check ingredients
+  if(empty($_POST['ingredients'])){
+    echo 'At least one ingredient is required <br />';
+  } else {
+    echo htmlspecialchars($_POST['ingredients']);
+  }
+
+  //end of POST check
+
 }
 
 ?>
